11302ca656d5e69aedef1ff76679e72f47aba762
cmin <cymmint@qq.com>
2018-01-31 15:07:09 +0800

    Site updated: 2018-01-31 15:07:02

---
+++ js/src/schemes/pisces.js
@@ -0,0 +1,7 @@
+/***!
+ * origin: 摘星少年个人博客。
+ * createtime: 2018-01-31
+ * author: cmin
+ * Copyright(c) 2018 http://cmin.me/
+ */
+$(document).ready(function(){var $headerInner=$(".header-inner"),$sidebar=$("#sidebar"),getSidebarTop=function(){return $headerInner.height()+10},setSidebarMarginTop=function(sidebarTop){return $sidebar.css({"margin-top":sidebarTop})},mql=window.matchMedia("(min-width: 991px)");setSidebarMarginTop(getSidebarTop()).show(),mql.addListener(function(e){e.matches&&setSidebarMarginTop(getSidebarTop())})});
